f4ad34df1b80d8c06ee9243df0ea56b201b72aef
Ndahiroloicke <ndahiroloicke@gmail.com>
2024-03-13 14:14:40 +0200

    changes

--- converter.js
+++ converter.js
@@ -14,6 +14,7 @@ window.speechSynthesis.onvoiceschanged = () => {
 };
 
 
+
 document.querySelector("button").addEventListener("click",() => {
     speech.text = document.querySelector("textarea").value
     window.speechSynthesis.speak(speech)
